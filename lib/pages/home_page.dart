4819a89076f1c51d17da451426d47af722610896
Abraham Oj <abrahamojo14@gmail.com>
2024-10-13 17:07:41 +0100

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/utilities/dialogue_box.dart';
+import '../utilities/todo_list.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List toDoList = [
+    ['Make Tutorial', false],
+    ['Do Exercise', true],
+  ];
+
+  void checkedBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogueBox();
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: const Text(
+          'TO DO',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkedBoxChanged(value, index),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
